ae9d2f2479c4c1c688144e3c93dec07976357611
Shivam Patel <shivampatel066@gmail.com>
2019-11-30 15:43:24 +0530

    Initial Commit

---
+++ RollofaDice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RollofaDice
+//
+//  Created by Shivam Patel on 30/11/19.
+//  Copyright © 2019 Shivam Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
